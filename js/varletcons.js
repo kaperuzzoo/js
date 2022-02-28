9916e3fb708b7fccc2cea1659c4df2b7c5680f6f
david vargas <kaperuzzoo@gmail.com>
2022-02-28 12:25:35 +0100

    porfin-sali

---
+++ js/varletcons.js
@@ -0,0 +1,33 @@
+console.log("Var VS Let VS Const")
+
+
+let nombre = "David";
+let apellido = "Vargas";
+let apodo = new String("Kaperuzo");
+
+console.log(nombre,apellido,apodo);
+console.log(apodo[5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
